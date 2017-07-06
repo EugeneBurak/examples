d33b61f1296a91720c22bbe869dc66240ab4ed17
Evgeniy <bea@sgsoft.com.ua>
2017-07-06 11:52:37 +0300

    Added a few code caps in the Main

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,11 @@ public class Main {
         Integer a4 = 5;
         Integer a5 = 5;
         Integer a6 = new Integer(5);
+        Integer a = 133;
+        Integer b = 133;
+        Integer d = 111;
+        Integer c = 111;
+
         if (a1.equals(a2))  {
             System.out.println("a1 equals a2 - true");
         } else  {
@@ -25,7 +30,7 @@ public class Main {
         if (a1 == a3)   {
             System.out.println("==");
         }   else    {
-            System.out.println("!=");
+            System.out.println("a1 != a3");
         }
         if (a4 == a5)   {
             System.out.println("a4 == a5");
@@ -35,8 +40,15 @@ public class Main {
         if (a4 == a6)   {
             System.out.println("a4 == a6");
         }   else    {
-            System.out.println("!=");
+            System.out.println("a4 != a6");
         }
-	// write your code here
+        System.out.println();
+        System.out.print("a == b -> ");
+        System.out.println(a==b);
+        System.out.print("d == c -> ");
+        System.out.println(d==c);
+        System.out.println();
+        System.out.println("3 + 5 = " + 3 + 5);         //3 + 5 = 35
+
     }
 }
